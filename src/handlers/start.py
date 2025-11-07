abd3ba967d8d2b422559e8007b205e900075edcc
madonnaprayer <adriaanmenschikow@gmail.com>
2025-11-07 22:23:21 +0100

    added basic inline buttons

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -5,7 +5,7 @@ from src.handlers.buttons.menu_keyboard import build_main_menu
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
-        "👋 Welcome to *tick!* Your personal to-do bot.\n\n"
+        " Welcome to *tick!* 👋\nYour personal to-do bot.\n\n"
         "Use the buttons below to get started:",
         parse_mode="Markdown",
         reply_markup=build_main_menu(),
